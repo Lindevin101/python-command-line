62d465febc033af967090344c220c1237639933c
dlin <lindevin101@gmail.com>
2023-12-03 18:26:18 -0500

    created nba.py with player name and team name

---
+++ nba.py
@@ -0,0 +1,166 @@
+nbaplayer = [
+  {
+    "name": "Kobe Bryant",
+    "team": "Lakers" 
+  },
+   {
+    "name": "LeBron James",
+    "team": "Lakers"
+ },
+ {
+    "name": "Kevin Durant",
+    "team": "Suns"
+ },
+ {
+    "name": "Anthony Davis",
+    "team": "Lakers"
+ },
+ {
+    "name": "James Harden",
+    "team": "Clippers"
+ },
+ {
+    "name": "Chris Paul",
+    "team": "Warriors"
+ },
+ {
+    "name": "Kawhi Leonard",
+    "team": "Clippers"
+ },
+ {
+    "name": "Damian Lillard",
+    "team": "Bucks"
+ },
+ {
+    "name": "Russell Westbrook",
+    "team": "Clippers"
+ },
+ {
+    "name": "Stephen Curry",
+    "team": "Warriors"
+ },
+ {
+    "name": "Kyrie Irving",
+    "team": "Mavericks"
+ },
+ {
+    "name": "Jayson Tatum",
+    "team": "Celtics"
+ },
+ {
+    "name": "Giannis Antetokounmpo",
+    "team": "Bucks"
+ },
+ {
+    "name": "Nikola Jokic",
+    "team": "Nuggets"
+ },
+ {
+    "name": "Paul George",
+    "team": "Clippers"
+ },
+ {
+    "name": "Jordan Pool",
+    "team": "Wizards"
+ },
+ {
+    "name": "DeMar DeRozan",
+    "team": "Bulls"
+ },
+ {
+    "name": "Klay Thompson",
+    "team": "Warriors"
+ },
+ {
+    "name": "Rudy Gobert",
+    "team": "Timberwolves"
+ },
+ {
+    "name": "Luka Doncic",
+    "team": "Mavericks"
+ },
+ {
+    "name": "Joel Embiid",
+    "team": "76ers"
+ },
+ {
+    "name": "Karl-Anthony Towns",
+    "team": "Timberwolves"
+ },
+ {
+    "name": "Jimmy Butler",
+    "team": "Heat"
+ },
+ {
+    "name": "Kristaps Porzingis",
+    "team": "Celtics"
+ },
+ {
+    "name": "Donovan Mitchell",
+    "team": "Cavaliers"
+ },
+ {
+    "name": "Bam Adebayo",
+    "team": "Heat"
+ },
+ {
+    "name": "Domantas Sabonis",
+    "team": "Kings"
+ },
+ {
+    "name": "Zion Williamson",
+    "team": "Pelicans"
+ },
+ {
+    "name": "De'Aaron Fox",
+    "team": "Kings"
+ },
+ {
+    "name": "RJ Barrett",
+    "team": "Knicks"
+ },
+ {
+    "name": "Brandon Ingram",
+    "team": "Pelicans"
+ },
+ {
+    "name": "Trae Young",
+    "team": "Hawks"
+ },
+ {
+    "name": "Patrick Beverley",
+    "team": "76ers"
+ },
+ {
+    "name": "Buddy Hield",
+    "team": "Pacers"
+ },
+ {
+    "name": "Mike Conley",
+    "team": "TImberwolves"
+ },
+ {
+    "name": "Chris Boucher",
+    "team": "Raptors"
+ },
+ {
+    "name": "Gordon Hayward",
+    "team": "Hornets"
+ },
+ {
+    "name": "Rudy Gobert",
+    "team": "Timberwolves"
+ },
+ {
+    "name": "Tobias Harris",
+    "team": "76ers"
+ },
+ {
+    "name": "Fred VanVleet",
+    "team": "Rockets"
+ },
+ {
+    "name": "Andrew Wiggins",
+    "team": "Warriors"
+ }
+ ]
